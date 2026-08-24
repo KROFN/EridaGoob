e195405675149eb3e6183d8f38cb7751d352a7ff
Lytheriia <289052288+Lytheriia@users.noreply.github.com>
2026-08-24 10:19:33 +0200

    Revert "Minor fix"
    This reverts commit 73827e37242c4175df0e79a6d0a3ec19f0d21ca1.

--- Content.Shared/Administration/AdminFlagsHelper.cs
+++ Content.Shared/Administration/AdminFlagsHelper.cs
@@ -68,7 +68,7 @@ namespace Content.Shared.Administration
             var flags = AdminFlags.None;
             foreach (var name in names)
             {
-                if (!NameFlagsMap.TryGetValue(name.ToLower(), out var value)) // Erida
+                if (!NameFlagsMap.TryGetValue(name, out var value))
                 {
                     throw new ArgumentException($"Invalid admin flag name: {name}");
                 }
